5a99189024d47db8fb3b70b33418de74608e1c36
Ahmed Khaled <36384512+AhmedKhaledRe@users.noreply.github.com>
2020-04-22 22:51:34 +0200

    passport and google strategy intialize with keys

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 const express = require('express');
 const app = express();
+const passport = require('passport');
+const GoogleStartegy = require('passport-google-oauth20').Strategy;
 const port = process.env.PORT || 5000;
-const www = process.env.WWW || './';
 
-app.use(express.static(www));
-console.log(`serving ${www}`);
-
-app.get('/', (req, res) => {
-    res.send({hi: 'there'});
-});
-
-app.get('*', (req, res) => {
-    res.sendFile(`index.html`, { root: www });
-});
+passport.use(new GoogleStartegy());
 
 app.listen(port, () => console.log(`listening on http://localhost:${port}`));
