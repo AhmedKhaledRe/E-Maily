fd7a7a80c8effda5fcee11757527c5ed06226db0
Ahmed Khaled <36384512+AhmedKhaledRe@users.noreply.github.com>
2020-05-18 01:31:04 +0200

    adding navBar and actions , stores, reducers, stripe

---
+++ client/src/components/Header.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+
+class Header extends Component {
+
+    renderContent() {
+        switch (this.props.auth) {
+            case null:
+                return;
+            case false:
+                return <li><a href="/auth/google">Login With Google</a></li>;
+            default:
+                return <li><a href="/api/logout">Logout</a></li>;
+        }
+    }
+
+    render() {
+        return (
+            <nav>
+                <div className="nav-wrapper" style={{ paddingLeft: 20 , paddingRight: 20}}>
+                    <Link to={this.props.auth000000087 ? "/surveys" : "/"} className="left brand-logo">
+                        E-Maily
+                    </Link>
+                    <ul id="nav-mobile" className="right hide-on-med-and-down">
+                        {this.renderContent()}
+                    </ul>
+                </div>
+            </nav>
+        );
+    }
+}
+
+function mapStateToProps({ auth }) {
+    return { auth };
+};
+
+export default connect(mapStateToProps)(Header);
---
+++ client/src/components/Landing.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Landing = () => {
+    return (
+        <div style={{ textAlign: "center" }}>
+            <h1>
+                E-Maily!
+            </h1>
+            Collect FeedBack from your users
+        </div>
+    );
+};
+
+export default Landing;
