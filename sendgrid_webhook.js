aa4641a33ef2ab38bdf3dac5b8df95e284ec0c4d
Ahmed Khaled <36384512+AhmedKhaledRe@users.noreply.github.com>
2020-06-09 17:20:38 +0200

    F3

--- sendgrid_webhook.js
+++ sendgrid_webhook.js
@@ -1,7 +1,4 @@
-const localtunnel = require('localtunnel');
-
-(async () => {
-  const tunnel = await localtunnel({ port: 5000 },{ subdomain: 'lairjgliargli' }, function(err, tunnel) {
-    console.log('LT running');
-  });
-})();
+var localtunnel = require('localtunnel');
+localtunnel(5000, { subdomain: 'emaily' }, function(err, tunnel) {
+  console.log('LT running');
+});
